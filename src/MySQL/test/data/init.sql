478909e68a7fe7e3f501cac24de7a346c55d96f9
Loki Astari <Loki.Astari@gmail.com>
2015-06-30 01:02:57 -0700

    Remember to use the test DB

--- src/MySQL/test/data/init.sql
+++ src/MySQL/test/data/init.sql
@@ -3,6 +3,8 @@ show databases;
 DROP    DATABASE IF EXISTS test;
 CREATE  DATABASE IF NOT EXISTS test;
 
+USE test;
+
 CREATE TABLE People ( ID  INTEGER, Name VARCHAR(64), Age  SMALLINT, Sex  CHAR(1), Height DOUBLE);
 
 INSERT INTO  People (ID, Name, Age, Sex, Height) VALUES (14, "Tom Smith", 32, 'M', 56.34);
