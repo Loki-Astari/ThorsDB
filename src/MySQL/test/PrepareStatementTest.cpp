62dceab6fea0cdf0309651d7e5f545512964b5ae
Loki Astari <Loki.Astari@gmail.com>
2015-07-07 08:38:15 -0700

    Add test to bind two parameters (failing)

--- src/MySQL/test/PrepareStatementTest.cpp
+++ src/MySQL/test/PrepareStatementTest.cpp
@@ -62,4 +62,24 @@ TEST(PrepareStatementTest, ExecuteWithBind)
                         });
     ASSERT_EQ(1, count);
 }
+TEST(PrepareStatementTest, ExecuteWithBindTwo)
+{
+    using namespace ThorsAnvil;
+    std::map<std::string, std::string>      options;
+    SQL::Connection     connection("mysql://" THOR_TESTING_MYSQL_HOST,
+                                    THOR_TESTING_MYSQL_USER,
+                                    THOR_TESTING_MYSQL_PASS,
+                                    THOR_TESTING_MYSQL_DB,
+                                    options);
+
+
+    SQL::Statement      statement(connection, "SELECT * FROM People where sex=? or age=?"); //select * from People;
+    long                count = 0;
+    statement.execute(SQL::Bind("M",28), [&count](int id, std::string name, short age, std::string sex, double height)
+                        {
+                            std::cout << "Got: " << id << " : " << name << " : " << age << " : " << sex << " : " << height << "\n";
+                            ++count;
+                        });
+    ASSERT_EQ(1, count);
+}
 
