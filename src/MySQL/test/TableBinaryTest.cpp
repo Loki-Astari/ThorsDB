010613e03df59e8436f3ecddcd304374702234e7
Loki Astari <Loki.Astari@gmail.com>
2017-03-06 23:40:49 -0800

    Add Unit test for VARBINARY

--- src/MySQL/test/TableBinaryTest.cpp
+++ src/MySQL/test/TableBinaryTest.cpp
@@ -13,5 +13,6 @@
 
 // Bit Values can only be read into "unsigned integer types"a
 TEST(TableBinaryTest, ReadBinary)      {typeGoodTest<std::string>("B",                               "SELECT B1 from BinaryTypes WHERE Id=1");} // BINARY   => MYSQL_TYPE_STRING  (its like char)
+TEST(TableBinaryTest, ReadVarBinary)   {typeGoodTest<std::string>("A variable binary string",        "SELECT B2 from BinaryTypes WHERE Id=1");} // VARBINARY=> MYSQL_TYPE_VAR_STRING (its like varchar)
 
 
