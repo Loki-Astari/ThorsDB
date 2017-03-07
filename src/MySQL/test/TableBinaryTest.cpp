5b9336b0063f626a5a9134e2cfcca74de7d0020a
Loki Astari <Loki.Astari@gmail.com>
2017-03-06 23:48:33 -0800

    Added all the BLOB types.
        They seem to be the same as the TEXT types

--- src/MySQL/test/TableBinaryTest.cpp
+++ src/MySQL/test/TableBinaryTest.cpp
@@ -12,7 +12,11 @@
 
 
 // Bit Values can only be read into "unsigned integer types"a
-TEST(TableBinaryTest, ReadBinary)      {typeGoodTest<std::string>("B",                               "SELECT B1 from BinaryTypes WHERE Id=1");} // BINARY   => MYSQL_TYPE_STRING  (its like char)
-TEST(TableBinaryTest, ReadVarBinary)   {typeGoodTest<std::string>("A variable binary string",        "SELECT B2 from BinaryTypes WHERE Id=1");} // VARBINARY=> MYSQL_TYPE_VAR_STRING (its like varchar)
+TEST(TableBinaryTest, ReadBinary)      {typeGoodTest<std::string>("B",                               "SELECT B1 from BinaryTypes WHERE Id=1");} // BINARY   => MYSQL_TYPE_STRING     (its like CHAR)
+TEST(TableBinaryTest, ReadVarBinary)   {typeGoodTest<std::string>("A variable binary string",        "SELECT B2 from BinaryTypes WHERE Id=1");} // VARBINARY=> MYSQL_TYPE_VAR_STRING (its like VARCHAR)
+TEST(TableBinaryTest, ReadTinyBlob)    {typeGoodTest<std::string>("A Tiny Blob",                     "SELECT B3 from BinaryTypes WHERE Id=1");} // TINYBLOB => MYSQL_TYPE_BLOB       (its like TINYTEXT)
+TEST(TableBinaryTest, ReadBlob)        {typeGoodTest<std::string>("A Normal Blob",                   "SELECT B4 from BinaryTypes WHERE Id=1");} // BLOB     => MYSQL_TYPE_BLOB       (its like TEXT)
+TEST(TableBinaryTest, ReadMedBlob)     {typeGoodTest<std::string>("A Medium Sized Blob",             "SELECT B5 from BinaryTypes WHERE Id=1");} // MEDIUMBLOB=> MYSQL_TYPE_BLOB      (its like MEDIUMTEXT)
+TEST(TableBinaryTest, ReadLongBlob)    {typeGoodTest<std::string>("This is the best I can do for long Blob","SELECT B6 from BinaryTypes WHERE Id=1");} // LONGBLOB => MYSQL_TYPE_BLOB(its like LONGTEXT)
 
 
