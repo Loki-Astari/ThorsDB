b39116c258743c23911093f6bf9cdac88a7feac9
Loki Astari <Loki.Astari@gmail.com>
2017-03-23 21:07:16 -0700

    Get MySQL lib to use the new common Stream

---
+++ src/MySQL/MySQLStream.h
@@ -0,0 +1,25 @@
+#ifndef THORS_ANVIL_MYSQL_MYSQL_STREAM_H
+#define THORS_ANVIL_MYSQL_MYSQL_STREAM_H
+
+#include "ThorSQL/StreamSimple.h"
+
+namespace ThorsAnvil
+{
+    namespace MySQL
+    {
+
+class MySQLStream: public SQL::StreamSimple
+{
+    public:
+        MySQLStream(int fd)
+            : SQL::StreamSimple(fd)
+        {}
+        MySQLStream(std::string const& host, int port = 3306)
+            : SQL::StreamSimple(host, port ? port : 3306)
+        {}
+};
+
+    }
+}
+
+#endif
