870765454da8cbb729f0b9b3724dfe16002b5aa1
Loki Astari <Loki.Astari@gmail.com>
2017-03-29 23:00:58 -0700

    Add unit test for all connection possibilities
        Simulate returning types we don't support for authentication

---
+++ src/Postgres/test/ConnectionSimulationErrorTest.cpp
@@ -0,0 +1,62 @@
+#include "Connection.h"
+#include "ConectReader.h"
+#include "ConectWriter.h"
+#include "ConectReader.h"
+#include "PackageBuffer.h"
+#include "PostgresStream.h"
+#include "Messages.h"
+#include "test/MockStream.h"
+#include "gtest/gtest.h"
+
+using ThorsAnvil::Postgres::Connection;
+using ThorsAnvil::Postgres::ConectReader;
+using ThorsAnvil::Postgres::ConectWriter;
+using ThorsAnvil::Postgres::PackageBuffer;
+using ThorsAnvil::Postgres::PostgresStream;
+
+TEST(ConnectionSimulationErrorTest, UnknownResposeCode)
+{
+    // area that we write to
+    unsigned char   result[1024];
+    // Simulated replies from the server
+    char const      data[] = "E"
+                             "\x00\x00\x00\x87"     // size
+                             "S" "S 001\0"          // 7
+                             "C" "C 002\0"          // 7    14
+                             "M" "M 003\0"          // 7    21
+                             "D" "D 004\0"          // 7    28
+                             "H" "H 005\0"          // 7    35
+                             "P" "P 006\0"          // 7    42
+                             "p" "p 007\0"          // 7    49
+                             "q" "q 008\0"          // 7    56
+                             "W" "W 009\0"          // 7    63
+                             "s" "s 010\0"          // 7    70
+                             "t" "t 011\0"          // 7    77
+                             "c" "c 012\0"          // 7    84
+                             "d" "d 013\0"          // 7    91
+                             "n" "n 014\0"          // 7    98
+                             "F" "F 015\0"          // 7    105
+                             "L" "L 016\0"          // 7    112
+                             "R" "R 017\0"          // 7    119
+                             "Z" "Z Invalid\0"      // 11   130
+                             "\0";                  // 1    131
+
+    MockStream      buffer(data, sizeof(data), result);
+    PackageBuffer   package(buffer);
+    ConectWriter    writer(package);
+    ConectReader    reader(package);
+    bool            fail = false;
+
+    try
+    {
+        Connection      connection("test", "testPassword", "test", {}, reader, writer);
+    }
+    catch(std::runtime_error const& e)
+    {
+        fail = true;
+        std::string message  = e.what();
+    }
+    ASSERT_EQ(true, fail);
+}
+
+
---
+++ src/Postgres/test/ConnectionSimulationTest.cpp
@@ -0,0 +1,256 @@
+
+#include "Connection.h"
+#include "ConectReader.h"
+#include "ConectWriter.h"
+#include "ConectReader.h"
+#include "PackageBuffer.h"
+#include "PostgresStream.h"
+#include "Messages.h"
+#include "test/MockStream.h"
+#include "gtest/gtest.h"
+
+using ThorsAnvil::Postgres::Connection;
+using ThorsAnvil::Postgres::ConectReader;
+using ThorsAnvil::Postgres::ConectWriter;
+using ThorsAnvil::Postgres::PackageBuffer;
+using ThorsAnvil::Postgres::PostgresStream;
+
+TEST(ConnectionSimulationTest, AuthenticationKerberosV5)
+{
+    // area that we write to
+    unsigned char   result[1024];
+    // Simulated replies from the server
+    char const      data[] = "R"
+                             "\x00\x00\x00\x08"     // size
+                             "\x00\x00\x00\x02";    // AuthenticationKerberosV5 code required.
+    MockStream      buffer(data, sizeof(data), result);
+    PackageBuffer   package(buffer);
+    ConectWriter    writer(package);
+    ConectReader    reader(package);
+    bool            fail = false;
+
+    try
+    {
+        Connection      connection("test", "testPassword", "test", {}, reader, writer);
+    }
+    catch(std::exception const& e)
+    {
+        fail = true;
+        std::string message = e.what();
+        std::size_t find    = message.find("Kerberos");
+
+        ASSERT_NE(std::string::npos, find);
+    }
+    ASSERT_EQ(true, fail);
+}
+TEST(ConnectionSimulationTest, AuthenticationCleartextPassword)
+{
+    // area that we write to
+    unsigned char   result[1024];
+    // Simulated replies from the server
+    char const      data[] = "R"
+                             "\x00\x00\x00\x08"     // size
+                             "\x00\x00\x00\x03";    // AuthenticationCleartextPassword code required.
+    MockStream      buffer(data, sizeof(data), result);
+    PackageBuffer   package(buffer);
+    ConectWriter    writer(package);
+    ConectReader    reader(package);
+    bool            fail = false;
+
+    try
+    {
+        Connection      connection("test", "testPassword", "test", {}, reader, writer);
+    }
+    catch(std::exception const& e)
+    {
+        fail = true;
+        std::string message = e.what();
+        std::size_t find    = message.find("Cleartext Password:");
+
+        ASSERT_NE(std::string::npos, find);
+    }
+    ASSERT_EQ(true, fail);
+}
+TEST(ConnectionSimulationTest, AuthenticationSCMCredential)
+{
+    // area that we write to
+    unsigned char   result[1024];
+    // Simulated replies from the server
+    char const      data[] = "R"
+                             "\x00\x00\x00\x08"     // size
+                             "\x00\x00\x00\x06";    // AuthenticationSCMCredential code required.
+    MockStream      buffer(data, sizeof(data), result);
+    PackageBuffer   package(buffer);
+    ConectWriter    writer(package);
+    ConectReader    reader(package);
+    bool            fail = false;
+
+    try
+    {
+        Connection      connection("test", "testPassword", "test", {}, reader, writer);
+    }
+    catch(std::exception const& e)
+    {
+        fail = true;
+        std::string message = e.what();
+        std::size_t find    = message.find("SCM Credential:");
+
+        ASSERT_NE(std::string::npos, find);
+    }
+    ASSERT_EQ(true, fail);
+}
+TEST(ConnectionSimulationTest, AuthenticationGSS)
+{
+    // area that we write to
+    unsigned char   result[1024];
+    // Simulated replies from the server
+    char const      data[] = "R"
+                             "\x00\x00\x00\x08"     // size
+                             "\x00\x00\x00\x07";    // AuthenticationGSS code required.
+    MockStream      buffer(data, sizeof(data), result);
+    PackageBuffer   package(buffer);
+    ConectWriter    writer(package);
+    ConectReader    reader(package);
+    bool            fail = false;
+
+    try
+    {
+        Connection      connection("test", "testPassword", "test", {}, reader, writer);
+    }
+    catch(std::exception const& e)
+    {
+        fail = true;
+        std::string message = e.what();
+        std::size_t find    = message.find("GSS:");
+
+        ASSERT_NE(std::string::npos, find);
+    }
+    ASSERT_EQ(true, fail);
+}
+TEST(ConnectionSimulationTest, AuthenticationSSPI)
+{
+    // area that we write to
+    unsigned char   result[1024];
+    // Simulated replies from the server
+    char const      data[] = "R"
+                             "\x00\x00\x00\x08"     // size
+                             "\x00\x00\x00\x09";    // AuthenticationSSPI code required.
+    MockStream      buffer(data, sizeof(data), result);
+    PackageBuffer   package(buffer);
+    ConectWriter    writer(package);
+    ConectReader    reader(package);
+    bool            fail = false;
+
+    try
+    {
+        Connection      connection("test", "testPassword", "test", {}, reader, writer);
+    }
+    catch(std::exception const& e)
+    {
+        fail = true;
+        std::string message = e.what();
+        std::size_t find    = message.find("SSPI:");
+
+        ASSERT_NE(std::string::npos, find);
+    }
+    ASSERT_EQ(true, fail);
+}
+TEST(ConnectionSimulationTest, AuthenticationGSSContinue)
+{
+    // area that we write to
+    unsigned char   result[1024];
+    // Simulated replies from the server
+    char const      data[] = "R"
+                             "\x00\x00\x00\x08"     // size
+                             "\x00\x00\x00\x08";    // AuthenticationGSSContinue code required.
+    MockStream      buffer(data, sizeof(data), result);
+    PackageBuffer   package(buffer);
+    ConectWriter    writer(package);
+    ConectReader    reader(package);
+    bool            fail = false;
+
+    try
+    {
+        Connection      connection("test", "testPassword", "test", {}, reader, writer);
+    }
+    catch(std::exception const& e)
+    {
+        fail = true;
+        std::string message = e.what();
+        std::size_t find    = message.find("GSS Continue:");
+
+        ASSERT_NE(std::string::npos, find);
+    }
+    ASSERT_EQ(true, fail);
+}
+
+TEST(ConnectionSimulationTest, UnknownResposeCode)
+{
+    // area that we write to
+    unsigned char   result[1024];
+    // Simulated replies from the server
+    char const      data[] = "R"
+                             "\x00\x00\x00\x08"     // size
+                             "\x00\x00\x00\xFF";    // AuthenticationGSSContinue code required.
+    MockStream      buffer(data, sizeof(data), result);
+    PackageBuffer   package(buffer);
+    ConectWriter    writer(package);
+    ConectReader    reader(package);
+
+    ASSERT_THROW(
+        Connection      connection("test", "testPassword", "test", {}, reader, writer),
+        std::domain_error
+    );
+}
+TEST(ConnectionSimulationTest, UnknownMessageType)
+{
+    // area that we write to
+    unsigned char   result[1024];
+    // Simulated replies from the server
+    char const      data[] = "Z"
+                             "\x00\x00\x00\x08"     // size
+                             "\x00\x00\x00\xFF";    // AuthenticationGSSContinue code required.
+    MockStream      buffer(data, sizeof(data), result);
+    PackageBuffer   package(buffer);
+    ConectWriter    writer(package);
+    ConectReader    reader(package);
+
+    ASSERT_THROW(
+        Connection      connection("test", "testPassword", "test", {}, reader, writer),
+        std::domain_error
+    );
+}
+TEST(ConnectionSimulationTest, InvalidMessageType)
+{
+    // area that we write to
+    unsigned char   result[1024];
+    // Simulated replies from the server
+    char const      data[] = "1"
+                             "\x00\x00\x00\x04";     // size
+    MockStream      buffer(data, sizeof(data), result);
+    PackageBuffer   package(buffer);
+    ConectWriter    writer(package);
+    ConectReader    reader(package);
+
+    ASSERT_THROW(
+        Connection      connection("test", "testPassword", "test", {}, reader, writer),
+        std::domain_error
+    );
+}
+TEST(ConnectionSimulationTest, PrintStartupMessage)
+{
+    ThorsAnvil::Postgres::StartupMessage message("TheUser", "ADBWithData", {{"Map", "Key"}});
+    std::stringstream output;
+    output << message;
+
+    std::string outputMessage = output.str();
+
+    std::size_t nameMess = outputMessage.find("StartupMessage");
+    std::size_t username = outputMessage.find("TheUser");
+    std::size_t database = outputMessage.find("ADBWithData");
+
+    ASSERT_NE(std::string::npos, nameMess);
+    ASSERT_NE(std::string::npos, username);
+    ASSERT_NE(std::string::npos, database);
+}
+
