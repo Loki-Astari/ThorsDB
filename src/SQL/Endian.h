654b5ef7e5cb97d12e3691523845440f4fa7f6ea
Loki Astari <Loki.Astari@gmail.com>
2017-03-30 08:54:44 -0700

    Fix Endian bug

--- src/SQL/Endian.h
+++ src/SQL/Endian.h
@@ -1,22 +1,25 @@
 #ifndef THORSANVIL_UTIL_ENDIAN_H
 #define THORSANVIL_UTIL_ENDIAN_H
 
+#include <type_traits>
 #include <arpa/inet.h>
 
+#ifdef htonll
+#undef htonll
+#undef ntohll
+#endif
+
 #ifndef htonll
-
-enum ThorEndianTestValues
+#if THOR_ENDIAN_BIG
+inline std::uint64_t htonll(std::uint64_t value)
 {
-    O32_LITTLE_ENDIAN   = 0x03020100ul,
-    O32_BIG_ENDIAN      = 0x00010203ul,
-    O32_PDP_ENDIAN      = 0x01000302ul
-};
-
-static constexpr union { unsigned char bytes[4]; uint32_t value; } thorHostOrderCheck = { { 0, 1, 2, 3 } };
-
-#define O32_HOST_ORDER (o32_host_order.value)
-
-#if O32_HOST_ORDER == O32_LITTLE_ENDIAN
+    return value;
+}
+inline std::uint64_t ntohll(std::uint64_t value)
+{
+    return value;
+}
+#else
 inline std::uint64_t htonll(std::uint64_t value)
 {
     uint64_t  result;
@@ -27,18 +30,16 @@ inline std::uint64_t htonll(std::uint64_t value)
     outputValue[1] = htonl(inputValue[0]);
     return result;
 }
-inline std::uint64_t ntohll(std::uint64_t value) {return htonll(value);}
-#elif O32_HOST_ORDER == O32_BIG_ENDIAN
-inline std::uint64_t htonll(std::uint64_t value)
-{
-    return value;
-}
 inline std::uint64_t ntohll(std::uint64_t value)
 {
-    return value;
+    uint64_t  result;
+    uint32_t* inputValue    = reinterpret_cast<uint32_t*>(&value);
+    uint32_t* outputValue   = reinterpret_cast<uint32_t*>(&result);
+
+    outputValue[0] = htonl(inputValue[1]);
+    outputValue[1] = htonl(inputValue[0]);
+    return result;
 }
-#elif O32_HOST_ORDER == O32_PDP_ENDIAN
-#error "Please write a htonll() for machines of PDP Endian"
 #endif
 #endif
 
