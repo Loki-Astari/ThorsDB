1a89f7976d25c8e21f60de5c609c8f89f69dede2
Loki Astari <Loki.Astari@gmail.com>
2018-08-13 00:53:23 -0700

    Add some more test coverage.
        For RespPackageHandShake

--- src/MySQL/test/RespPackageEOFTest.cpp
+++ src/MySQL/test/RespPackageEOFTest.cpp
@@ -5,7 +5,6 @@
 #include "ConectReader.h"
 #include "RespPackageEOF.h"
 #include "test/MockStream.h"
-#include <iostream>
 
 using ThorsAnvil::SQL::Options;
 using ThorsAnvil::MySQL::ConectReader;
@@ -28,13 +27,12 @@ TEST(RespPackageEOFTest, DefaultEOFMessage)
 
     std::stringstream message;
     message << eof;
-    std::cerr << message.str() << "\n";
     EXPECT_NE(message.str(), "");
+    auto findHumanMassage = message.str().find("humanMessage()"); // The rest of the buffer
+    EXPECT_NE(std::string::npos, findHumanMassage);
     auto findWarning = message.str().find("warningCount(0x00000001)");
     EXPECT_NE(std::string::npos, findWarning);
     auto findStatus = message.str().find("statusFlag(0x00000003)");
     EXPECT_NE(std::string::npos, findStatus);
-    auto findHumanMassage = message.str().find("humanMessage()"); // The rest of the buffer
-    EXPECT_NE(std::string::npos, findHumanMassage);
 }
 
--- src/MySQL/test/RespPackageERRTest.cpp
+++ src/MySQL/test/RespPackageERRTest.cpp
@@ -28,13 +28,13 @@ TEST(RespPackageERRTest, DefaultERRMessage)
     message << eof;
 
     EXPECT_NE(message.str(), "");
+    auto findHumanMassage = message.str().find("humanMessage(Plop)"); // The rest of the buffer
+    EXPECT_NE(std::string::npos, findHumanMassage);
     auto findErrorCode = message.str().find("errorCode(35)");   // 0x23 first two byte in buffer
     EXPECT_NE(std::string::npos, findErrorCode);
     auto findHash = message.str().find("hash(A)");              // a third character in buffer
     EXPECT_NE(std::string::npos, findHash);
     auto findState = message.str().find("stateSQL(12345)");     // next 5 bytes
     EXPECT_NE(std::string::npos, findState);
-    auto findHumanMassage = message.str().find("humanMessage(Plop)"); // The rest of the buffer
-    EXPECT_NE(std::string::npos, findHumanMassage);
 }
 
