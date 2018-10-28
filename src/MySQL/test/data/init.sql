1104106cdd3b9ec8fe1a32c9dbd720b887e9a86b
Loki Astari <Loki.Astari@gmail.com>
2018-10-28 13:37:01 -0700

    Update the init to correctly install users

--- src/MySQL/test/data/init.sql
+++ src/MySQL/test/data/init.sql
@@ -2,10 +2,12 @@ show databases;
 
 DROP    DATABASE IF EXISTS test;
 DROP    USER IF EXISTS 'test'@'localhost';
+DROP    USER IF EXISTS 'ssluser'@'localhost';
+DROP    USER IF EXISTS 'ssluserEmpty'@'localhost';
 
 CREATE  DATABASE IF NOT EXISTS test;
 CREATE  USER 'test'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'testPassword';
-CREATE  USER 'test'@'localhost' IDENTIFIED BY 'testPassword';
+-- CREATE  USER 'test'@'localhost' IDENTIFIED BY 'testPassword';
 CREATE  USER 'ssluser'@'localhost' IDENTIFIED BY 'sslPassword';
 CREATE  USER 'ssluserEmpty'@'localhost' IDENTIFIED BY '';
 GRANT   ALL ON test.* TO 'test'@'localhost';
